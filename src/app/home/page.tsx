37b6468337f40af2d1d4480dd9ca9a4d008896a0
Pooria Mon <poriya.monfared@gmail.com>
2024-02-05 10:19:04 +0100

    Add a UI template framework

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
 import './style.css';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h3>Hello World!</h3>
+      <Button variant="contained">Hello world</Button>
     </main>
   );
 }
